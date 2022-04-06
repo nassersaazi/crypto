aea91e9d51bb85681a0406d1dcd1bbb1d4990559
nassersaazi <nassersnoop@gmail.com>
2022-04-06 16:53:13 +0300

    Set up the components layout

--- projects/cryptoverse/src/App.js
+++ projects/cryptoverse/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
-import { Navbar } from './components';
+import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
 import './App.css'
 const App = () => {
     return (
@@ -11,7 +11,18 @@ const App = () => {
                 <Navbar />
             </div>
             <div className="main">
-
+            <Layout>
+        <div className="routes">
+          <Routes>
+                <Route exact path="/" element={<Homepage />} />
+                <Route exact path="/exchanges" element={<Exchanges />} />
+                <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />} />
+                <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+                <Route exact path="/news" element={<News />} />
+                
+          </Routes>
+        </div>
+      </Layout>
             </div>
             <div className="footer">
 
